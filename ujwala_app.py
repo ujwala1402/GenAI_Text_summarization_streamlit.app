33093f09b1bca42fae3d2c0bd12c0a500e55be90
ujwala1402 <pranshupatne944@gmail.com>
2025-03-13 10:32:29 +0530

    Update ujwala_app.py

--- ujwala_app.py
+++ ujwala_app.py
@@ -16,7 +16,7 @@ st.title('🦜🔗 Text Summarization App')
 st.divider()  # 👈 Draws a horizontal rule
 # get API Key
 client = Groq(
-    api_key=st.secrets["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"],
+    api_key=st.secrets["GROO_API_KEY"],
 )
 def generate_response(txt):
     # Instantiate the LLM model
